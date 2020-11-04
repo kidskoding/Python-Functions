c601dc9a5e3e99152c7c6355e34b4d76f63d2445
Kids Koding <68204671+kidskoding@users.noreply.github.com>
2020-11-03 19:20:14 -0600

    Create discord5.py

---
+++ discord5.py
@@ -0,0 +1,5 @@
+#we can use the return key to allow the terminal to return a value to us
+
+def function(x): #defined function as x
+  return 5 + x #this is what is inside the function so the terminal will return 5 + x. x is 3 so terminal returns 8
+print(function(3)) #3 substitutes as x because x = 3
