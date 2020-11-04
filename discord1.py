e593e6dbeaa3d0396db51fa74f94c614c6b5a54e
Kids Koding <68204671+kidskoding@users.noreply.github.com>
2020-11-03 19:09:46 -0600

    Create discord1.py

---
+++ discord1.py
@@ -0,0 +1,3 @@
+def function(): #this right here defines the function by using the def keyword. I set a name for the function which was simply function and the two parentheses tell the terminal its a function. 
+  print("A function") #anything inside the function is what the terminal will display. In this scenario, the terminal will display A function  
+function() #this calls the function. Functions will only work when you call them.
