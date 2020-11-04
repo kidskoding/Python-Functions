2ac223e672d9433cde824eda96acfaa6adcd04b7
Kids Koding <68204671+kidskoding@users.noreply.github.com>
2020-11-03 19:12:30 -0600

    Create discord2.py

---
+++ discord2.py
@@ -0,0 +1,4 @@
+def function(fruit): #we pass information into functions called arguments or parameters
+  print(fruit + " juice") #I ask the terminal to print the argument fruit and juice
+function("orange") #fruit in this case is orange so what the terminal is going to do is that it will substitute orange with fruit and will give orange juice with a space in between
+#I included that space in between orange and juice by adding a space between quotation mark and juice in between
