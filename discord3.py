33e601b0e706b387ac4ea780446e8e3417c25f71
Kids Koding <68204671+kidskoding@users.noreply.github.com>
2020-11-03 19:15:49 -0600

    Create discord3.py

---
+++ discord3.py
@@ -0,0 +1,4 @@
+#if you are adding more than one argument then you must separate them with a comma
+def function(fruit, item):#since i added more than one argument, I put a space in between fruit and item. Those are both arguments
+  print(fruit + " " + item) #this creates a space between apple and pie with the two quotations in between. I did it this way because both fruit and item are arguments not strings so you don't want to put quotation marks on those words.
+function("apple", "pie") #I made apple = fruit and pie = item. So those will substitute in the print statement giving the result apple pie with a space
